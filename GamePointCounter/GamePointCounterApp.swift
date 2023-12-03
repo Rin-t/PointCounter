c2de66193bd550b039cb3e6bc25eb1e904084366
Rin-t <rin.t.4921@gmail.com>
2023-12-03 14:58:27 +0900

    Initial Commit

---
+++ GamePointCounter/GamePointCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  GamePointCounterApp.swift
+//  GamePointCounter
+//
+//  Created by Rin on 2023/12/03.
+//
+
+import SwiftUI
+
+@main
+struct GamePointCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
